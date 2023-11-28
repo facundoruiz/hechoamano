097619b43c483150925e6d85a0e387cd9cfa0c78
Facundo <facundoruiz@gmail.com>
2023-11-28 11:34:25 -0300

    ajustes

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# TEMPLATE (carga de Novedades)
-![Alt text](./src/ico/logo.svg "Cargade Novedades")
+# Hecho a Mano(Catalogo)
 
 ## Install por primera vez
 > `npm install`
@@ -12,30 +11,30 @@
 ## Paquetes/ dependencies
 
 
-    dependencies
+  ### dependencies:
 
     "@popperjs/core": "^2.11.8",
     "bootstrap": "^5.3.0",
+    "chokidar": "^3.5.3",
     "dotenv": "^16.1.4",
-
     "firebase": "^9.21.0",
     "mini-css-extract-plugin": "^2.4.4",
-    
-    "toastify-js": "^1.12.0"
+    "toastify-js": "^1.12.0",
+    "workbox-webpack-plugin": "^7.0.0"
 
-    devDependencies:
+  ###  devDependencies:
 
-     "autoprefixer": "^10.4.14",
+    "autoprefixer": "^10.4.14",
     "css-loader": "^6.8.1",
+    "gh-pages": "^5.0.0",
     "html-webpack-plugin": "^5.5.1",
     "postcss-loader": "^7.3.2",
     "sass": "^1.62.1",
     "sass-loader": "^13.3.1",
     "style-loader": "^3.3.3",
-    "webpack": "^5.85.0",
-    "webpack-cli": "^4.10.0",
-    "webpack-dev-server": "^4.15.0",
-      "gh-pages": "^5.0.0"
+    "webpack": "^5.89.0",
+    "webpack-cli": "^5.1.4",
+    "webpack-dev-server": "^4.15.0"
 
    
 
--- src/productos/productos_list.js
+++ src/productos/productos_list.js
@@ -1,6 +1,5 @@
 import {
-  onGetTasks,
-  saveTask
+  onGetTasks
 } from "./productos_crud.js"
 import {
   showMessage
@@ -29,7 +28,7 @@ export const showTareas = () => {
               <h6 class="descrip">${taks.descripcion}</h6>
               <div class="post-meta-single mt-3">
                   <ul>
-                      <li><i class="fa fa-clock-o"></i>${createdAt}</li>
+                      <li><i class="fa fa-mone"></i>${taks.precio}</li>
                   </ul>
               </div>
           </div>
@@ -46,67 +45,3 @@ export const showTareas = () => {
 }
 
 
-
-
-const taskForm = document.getElementById("taks-form");
-if(taskForm){
-
-  const btnAddTak = document.querySelector("#save-tak");
-  
-// Función para cuando hagan click en boton y  autenticar con Google y guardar el usuario en Firestore
-//btnAddTak.addEventListener("click", async (e) => {
-  taskForm.addEventListener("submit", async (e) => {
-
-    
-    e.preventDefault();
-    
-    console.log(taskForm["formFile"].files[0]);
-
-  if (saveTask( taskForm,file)) {
-    // show  message
-    taskForm.reset();
-    taskForm['nombre'].focus();
-    showMessage("pieza Guardada!");
-    //window.location.href = "./";
-  } else {
-    showMessage(" Errror", error);
-  }
-  
-  
-})
-
-}
-
-const DATE_UNITS = {
-  day: 86400,
-  hour: 3600,
-  minute: 60,
-  second: 1
-};
-
-const getSecondsDiff = timestamp => {
-  const now = new Date();
-  const timestampDate = timestamp instanceof Date ? timestamp : timestamp.toDate();
-  return (now.getTime() - timestampDate.getTime()) / 1000;
-};
-
-
-const getUnitAndValueDate = secondsElapsed => {
-  for (const [unit, secondsInUnit] of Object.entries(DATE_UNITS)) {
-    if (secondsElapsed >= secondsInUnit || unit === "second") {
-      const value = Math.floor(secondsElapsed / secondsInUnit) * -1;
-      return { value, unit };
-    }
-  }
-};
-
-const getTimeAgo = timestamp => {
-  if (!timestamp) {
-    return 'recien';
-  }
-  const rtf = new Intl.RelativeTimeFormat();
-
-  const secondsElapsed = getSecondsDiff(timestamp);
-  const { value, unit } = getUnitAndValueDate(secondsElapsed);
-  return rtf.format(value, unit);
-};
